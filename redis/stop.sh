5ef3aa0bedf6f3d19c9d7e209278eac110da7993
Yuki Adachi <yuki777@gmail.com>
2023-05-04 16:51:17 +0900

    Support json for redis

--- redis/stop.sh
+++ redis/stop.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 set -eu
 
-currentDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# Get format option
+format=""
+while getopts ":f:" opt; do
+  case ${opt} in
+  f)
+    format="$OPTARG"
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Option -$OPTARG requires an argument." 1>&2
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+currentDir="$(
+  cd "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
 cd $currentDir
 . functions.sh
 
@@ -20,4 +42,24 @@ exitIfNotRunningPort $optPort
 $dir/basedir/src/redis-cli -p $optPort shutdown
 [ -f "$dir/datadir/$optName/redis.port" ] && cp $dir/datadir/$optName/redis.port $dir/datadir/$optName/redis.port.last
 [ -f "$dir/datadir/$optName/redis.port" ] && rm -f $dir/datadir/$optName/redis.port
-echo Redis Successfully stopped. $optName $optVersion $optPort
+
+normalOutputs=""
+normalOutputs="${normalOutputs}Redis Successfully stopped. $optName $optVersion $optPort"
+
+jsonOutputs=""
+jsonOutputs="$jsonOutputs{
+  \"message\": \"Redis Successfully stopped.\",
+  \"name\": \"$optName\",
+  \"type\": \"redis\",
+  \"version\": \"$optVersion\",
+  \"port\": \"$optPort\",
+  \"dataDir\": \"$dir/datadir/$optName\",
+  \"confPath\": \"$dir/datadir/$optName/redis.conf\"
+}"
+
+# Output
+if [ "$format" = "json" ]; then
+  echo -e "${jsonOutputs}"
+else
+  echo -e "${normalOutputs}"
+fi
