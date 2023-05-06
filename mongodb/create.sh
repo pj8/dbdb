e33270efba7178c1a8ed10f363010603bc8e4d02
Yuki Adachi <yuki777@gmail.com>
2023-05-06 09:04:20 +0900

    Support json on mongodb

--- mongodb/create.sh
+++ mongodb/create.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -eu
 
+# Get format option
+format=""
+while getopts ":f:" opt; do
+  case ${opt} in
+  f)
+    format="$OPTARG"
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Option -$OPTARG requires an argument." 1>&2
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 currentDir="$(
   cd "$(dirname "$0")" >/dev/null 2>&1
   pwd -P
@@ -9,10 +28,10 @@ cd $currentDir
 
 if [ $# -eq 0 ]; then
   cat <<_EOT_
-# usage : $currentDir/create.sh {Name} {MongodbVersion} {Port}
-# e.g.  : $currentDir/create.sh mongo4-foo 4.4.10 27017
-# e.g.  : $currentDir/create.sh mongo5-bar 5.0.3  37017
-# e.g.  : $currentDir/create.sh mongo5-baz 5.0.3  random
+usage : $currentDir/create.sh {Name} {MongodbVersion} {Port}
+e.g.  : $currentDir/create.sh mongo4-foo 4.4.10 27017
+e.g.  : $currentDir/create.sh mongo5-bar 5.0.3  37017
+e.g.  : $currentDir/create.sh mongo5-baz 5.0.3  random
 _EOT_
   exit 1
 fi
@@ -37,10 +56,31 @@ mkdir -p $dir/datadir/$optName
 extractFile $dir $optFileName
 
 echo $optPort >$dir/datadir/$optName/mongodb.port.init
+echo "#mongod.conf" > $dir/datadir/$optName/mongod.conf
 
-echo "#mongod.conf" >$dir/datadir/$optName/mongod.conf
-echo "mongod.conf is here. $dir/datadir/$optName/mongod.conf"
 
-echo MongoDB Successfully created. $optName $optVersion $optPort
 cd $currentDir
-getCommands $optName $optVersion $optPort
+commands=$(getCommands $optName $optVersion $optPort $format)
+
+normalOutputs=""
+normalOutputs="${normalOutputs}mongod.conf is here. $dir/datadir/$optName/mongod.conf\n"
+normalOutputs="${normalOutputs}MongoDB Successfully created. $optName $optVersion $optPort\n"
+normalOutputs="${normalOutputs}$commands\n"
+
+jsonOutputs=""
+jsonOutputs="$jsonOutputs{
+  \"message\": \"MongoDB Successfully created.\",
+  \"name\": \"$optName\",
+  \"type\": \"mongodb\",
+  \"version\": \"$optVersion\",
+  \"port\": \"$optPort\",
+  \"dataDir\": \"$dir/datadir/$optName\",
+  \"confPath\": \"$dir/datadir/$optName/mongod.conf\"
+}"
+
+# Output
+if [ "$format" = "json" ]; then
+  echo -e "${jsonOutputs}"
+else
+  echo -e "${normalOutputs}"
+fi
--- mongodb/stop.sh
+++ mongodb/stop.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 set -eu
 
-currentDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# Get format option
+format=""
+while getopts ":f:" opt; do
+  case ${opt} in
+  f)
+    format="$OPTARG"
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Option -$OPTARG requires an argument." 1>&2
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+currentDir="$(
+  cd "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
 cd $currentDir
 . functions.sh
 
@@ -20,4 +42,24 @@ exitIfNotRunningPort $optPort
 [ -f "$dir/datadir/$optName/mongodb.pid" ] && kill $(cat $dir/datadir/$optName/mongodb.pid) && rm -f $dir/datadir/$optName/mongodb.pid
 [ -f "$dir/datadir/$optName/mongodb.port" ] && cp $dir/datadir/$optName/mongodb.port $dir/datadir/$optName/mongodb.port.last
 [ -f "$dir/datadir/$optName/mongodb.port" ] && rm -f $dir/datadir/$optName/mongodb.port
-echo MongoDB Successfully stopped. $optName $optVersion $optPort
+
+normalOutputs=""
+normalOutputs="${normalOutputs}MongoDB Successfully stopped. $optName $optVersion $optPort"
+
+jsonOutputs=""
+jsonOutputs="$jsonOutputs{
+  \"message\": \"MongoDB Successfully stopped.\",
+  \"name\": \"$optName\",
+  \"type\": \"mongodb\",
+  \"version\": \"$optVersion\",
+  \"port\": \"$optPort\",
+  \"dataDir\": \"$dir/datadir/$optName\",
+  \"confPath\": \"$dir/datadir/$optName/mongod.conf\"
+}"
+
+# Output
+if [ "$format" = "json" ]; then
+  echo -e "${jsonOutputs}"
+else
+  echo -e "${normalOutputs}"
+fi
