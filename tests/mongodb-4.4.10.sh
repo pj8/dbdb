a9d6a447aba0af66fe0bec573a00fb089576db80
Yuki Adachi <yuki777@gmail.com>
2023-05-07 11:41:10 +0900

    Improve tests

--- tests/mongodb-4.4.10.sh
+++ tests/mongodb-4.4.10.sh
@@ -3,49 +3,48 @@ set -aeuvx
 
 . ../lib/functions.sh
 
-type=postgresql
-version=13.2
+type=mongodb
+version=4.4.10
 
-cd ../$type
-rm -fr $type-*.tar.gz
-cd ..
+cd ../$type/..
 
 date=$(date +%Y%m%d%H%M%S)
 md5="md5"
 [ "$(getOS)" = "linux" ] && md5="md5sum"
 hash=$(echo "dbdb-$date" | $md5 | cut -d ' ' -f 1)
 
-echo "Test create"
+echo "# Test create"
 ./$type/create.sh dbdb-test-$hash $version random
-echo "Test port"
+echo "# Test port"
 ./$type/port.sh dbdb-test-$hash
-echo "Test start"
+echo "# Test start"
 ./$type/start.sh dbdb-test-$hash
-echo "Test status"
+echo "# Test status"
 ./$type/status.sh dbdb-test-$hash
-echo "Test restart"
+echo "# Test restart"
 ./$type/restart.sh dbdb-test-$hash
-echo "Test stop"
+echo "# Test stop"
 ./$type/stop.sh dbdb-test-$hash
-echo "Test delete"
+echo "# Test delete"
 ./$type/delete.sh dbdb-test-$hash
 
-echo "Test create"
+echo "# Test create"
 ./$type/create.sh -f json dbdb-test-$hash $version random | jq
-echo "Test port"
+echo "# Test port"
 ./$type/port.sh -f json dbdb-test-$hash | jq
-echo "Test start"
+echo "# Test start"
 ./$type/start.sh -f json dbdb-test-$hash | jq
-echo "Test status"
+echo "# Test status"
 ./$type/status.sh -f json dbdb-test-$hash | jq
-echo "Test restart"
+echo "# Test restart"
 ./$type/restart.sh -f json dbdb-test-$hash | jq
-echo "Test stop"
+echo "# Test stop"
 ./$type/stop.sh -f json dbdb-test-$hash | jq
-echo "Test delete"
+echo "# Test delete"
 ./$type/delete.sh -f json dbdb-test-$hash | jq
 
 ./dbdb.sh
 ./$type/create-start.sh -f json dbdb-test-$hash $version random | jq
 ./dbdb.sh -f json | jq
 ./$type/delete.sh dbdb-test-$hash
+./dbdb.sh -f json | jq
