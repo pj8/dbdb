fe3bac8e16cb7503dd12af3dc23986244e708fd7
Yuki Adachi <177159+yuki777@users.noreply.github.com>
2024-09-25 00:37:18 +0700

    Add memcached/connect.sh

---
+++ memcached/connect.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+currentDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $currentDir
+. functions.sh
+
+optName=$1
+
+exitIfNotExistVersion "$optName"
+optVersion=$(getVersionByName "$optName")
+
+exitIfNotExistPortFile "$optName" "$optVersion"
+optPort=$(getPortByName "$optName" "$optVersion")
+
+installDir=$(getInstallDir $(getType))
+dir=$installDir/versions/$optVersion
+
+exitIfNotExistDir $dir/datadir/$optName
+exitIfNotRunningPort $optPort
+
+echo "You can connect to the server by executing the following command."
+echo "telnet 127.0.0.1 $optPort"
